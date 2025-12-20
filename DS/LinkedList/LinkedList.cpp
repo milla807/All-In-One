f1472cbae3b01ce1a9bb38e3d77cba7197e6a024
Million <smashmilli70@gmail.com>
2025-12-20 23:48:51 +0300

    <Update add deletion and search element>

--- DS/LinkedList/LinkedList.cpp
+++ DS/LinkedList/LinkedList.cpp
@@ -33,10 +33,58 @@ void displayList(){
     }
     cout<<endl;
 }
+
+// function to search an element in the linked list
+void searchElement(int value){
+    node*temp = head;
+    while (temp != NULL)
+    {
+        if(temp->data == value){
+            cout<<"Element found in the list."<<endl;
+            return;
+        }
+        temp = temp->next;
+    }
+    
+} 
+
+// function to delete an element from the linked list
+void deleteElement(int value){
+    if(head == NULL){
+        cout<<"List is empty, cannot delete."<<endl;
+        return;
+    }
+    if(head->data == value){
+        node* temp = head;
+        head = head->next;
+        delete temp;
+        return;
+    }
+    node* current = head;
+    node* previous = NULL;
+    while(current != NULL && current->data != value){
+        previous = current;
+        current = current->next;
+    }
+    if(current == NULL){
+        cout<<"Element not found in the list."<<endl;
+        return;
+    }
+    previous->next = current->next;
+    delete current;
+}
+
 int main(){
     insertAtEnd(10);
     insertAtEnd(20);
     insertAtEnd(30);
+    insertAtEnd(40);
+    insertAtEnd(50);
+
+    searchElement(20); // Output: Element found in the list.
+    
+    deleteElement(40);
+    
     cout<<"Linked List: ";
     displayList(); // Output: 10 20 30
     return 0;
